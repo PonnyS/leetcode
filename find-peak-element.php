acdb0aa9ab0a313f9b388a0d363880f2ec002635
ruanshuaishuai <ruanshuaishuai@jd.com>
2020-01-20 17:29:53 +0800

    modify

---
+++ find-peak-element.php
@@ -0,0 +1,25 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function findPeakElement($nums) {
+        $left = 0;
+        $right = count($nums) - 1;
+        while ($left < $right) {
+            $mid = (int)floor(($left+$right)/2);
+            if ($nums[$mid] > $nums[$mid+1]) {
+                $right = $mid;
+            } else {
+                $left = $mid+1;
+            }
+        }
+        return $left;
+    }
+}
+
+$nums = [1,2,3,1];
+echo (new Solution())->findPeakElement($nums);
